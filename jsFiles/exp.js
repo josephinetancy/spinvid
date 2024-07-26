f66b379d3dda7679248ca60cf5fd94ad667cd852
josephinetancy <99933363+josephinetancy@users.noreply.github.com>
2024-07-26 08:26:01 -0700

    typo

--- jsFiles/exp.js
+++ jsFiles/exp.js
@@ -333,7 +333,7 @@ console.log("this is the" + round)
         timeline_variables: wheels,
         sample: {
             type: 'alternate_groups',
-            groups: [[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]], [[16,17,18,19]],
+            groups: [[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], [16,17,18,19]],
             randomize_group_order: true
         }
     };
